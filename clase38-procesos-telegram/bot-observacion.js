c31285356a3eb69a7a859d20cb2db35e41688781
YESENIA BOLAÑOS <yess1020x@gmail.com>
2026-06-09 23:14:38 +0000

    clase37

--- clase38-procesos-telegram/bot-observacion.js
+++ clase38-procesos-telegram/bot-observacion.js
@@ -4,25 +4,26 @@ const TelegramBot = require("node-telegram-bot-api");
 
 const token = process.env.TELEGRAM_BOT_TOKEN;
 const chatId = process.env.TELEGRAM_CHAT_ID;
-
-if (!token || !chatId) {
-  console.error("Faltan TELEGRAM_BOT_TOKEN o TELEGRAM_CHAT_ID en el archivo .env");
+if (!token) {
+  console.error("Falta TELEGRAM_BOT_TOKEN en el archivo .env");
   process.exit(1);
 }
 
-const bot = new TelegramBot(token, { polling: false });
+const bot = new TelegramBot(token, { polling: true });
 
-async function enviarMensaje() {
-  try {
+console.log("Bot en modo observacion. El proceso esta corriendo.");
+
+bot.on("message", async (msg) => {
+  console.log("Mensaje recibido:", msg.text);
+  if(msg.text =='hola')
+     try {
     await bot.sendMessage(
       chatId,
-      "hola mundo."
+      "hola como estan?."
     );
 
     console.log("Mensaje enviado correctamente a Telegram.");
   } catch (error) {
     console.error("Error al enviar mensaje:", error.message);
   }
-}
-
-enviarMensaje();
+});
